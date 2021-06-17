70111b15d79d90f987e980c48f5eb4ca0985ef53
FREEDOM <freewhynane62@gmail.com>
2021-06-17 07:30:14 +0000

    add component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Narbar from './component/Navbar/Navbar';
 import Carousel from './component/Carousel/Carousel';
 import Plans from './component/Plans/Plans';
 import Footer from './component/Footer/Footer';
-import './fonts';
 
 function App() {
   return (
