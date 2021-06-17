d231948cdaa9d93140942d92ca2ed28c3f5a2688
FREEDOM <freewhynane62@gmail.com>
2021-06-17 08:18:55 +0000

    add code to navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import logo from '../../Images/logo (1).png';
+import ghana from '../../Images/gh.png';
+import nigeria from '../../Images/nija.png';
+import kenya from '../../Images/kenya.png';
+import './Navbar.css'
+import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar navbar-default navbar-home">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <button
+                        type="button"
+                        className="navbar-toggle collapsed"
+                        data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand  logo" href="#">
+                        <img src={logo} className="img-responsive" />
+                    </a>
+                </div>
+
+                <div
+                    className="collapse navbar-collapse"
+                    id="bs-example-navbar-collapse-1"
+                >
+                    <ul className="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="#">ABOUT US</a>
+                        </li>
+                    </ul>
+                    <div className="nav navbar-nav navbar-center flag">
+                        <li>
+                            <a href="#">
+                                <img src={ghana} className="img-responsive" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src={nigeria} className="img-responsive" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src={kenya} className="img-responsive" />
+                            </a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
-}
+};
 
 export default Navbar;
