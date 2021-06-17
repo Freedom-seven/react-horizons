491c2076f6f232b03a1dc845a212d6e7178b0a2f
FREEDOM <freewhynane62@gmail.com>
2021-06-17 08:24:06 +0000

    previous and next symbol remove

--- src/component/Carousel/Carousel.jsx
+++ src/component/Carousel/Carousel.jsx
@@ -51,31 +51,6 @@ class Carousel extends Component {
                         <div className="carousel-caption"></div>
                     </div>
                 </div>
-
-                <a
-                    className="left carousel-control"
-                    href="#carousel-example-generic"
-                    role="button"
-                    data-slide="prev"
-                >
-                    <span
-                        className="glyphicon glyphicon-chevron-left"
-                        aria-hidden="true"
-                    ></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a
-                    className="right carousel-control"
-                    href="#carousel-example-generic"
-                    role="button"
-                    data-slide="next"
-                >
-                    <span
-                        className="glyphicon glyphicon-chevron-right"
-                        aria-hidden="true"
-                    ></span>
-                    <span className="sr-only">Next</span>
-                </a>
             </div>
         );
     }
