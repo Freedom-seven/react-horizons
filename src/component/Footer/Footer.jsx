cc5198b86028633f98a578935b4b215a05a07522
FREEDOM <freewhynane62@gmail.com>
2021-06-17 09:02:37 +0000

    add code to footer component

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-    render() {
-        return (
-            <div>
-                Footer
+  render() {
+    return (
+      <div className="footer">
+        <div className="container-fluid">
+          <div className="row footer-content">
+            <div className="col-md-4">
+              <address>
+                <strong>VISIT US IN KENYA</strong>
+                <br />
+                <span>Nairobi, Kenya</span>
+                <br />
+                Belgravi center, 14 Riverside drive,
+                <br />
+                4th floor,Off Riverside Drive
+                <br />
+                Hanover Centre, 14 Riverside Drive
+                <br />
+                6th floor, Off Riverside Drive
+                <br />
+              </address>
             </div>
-        );
-    }
+            <div className="col-md-4">
+              <address>
+                <strong>VISIT US IN GHANA</strong>
+                <br />
+                <span>Accra, Ghana</span>
+                <br />
+                One Airport square, Airport City
+                <br />
+                8th Floor
+                <br />
+              </address>
+            </div>
+            <div className="col-md-4">
+              <address>
+                <strong>VISIT US IN NIGERIA</strong>
+                <br />
+                <span>Lagos, Nigeria</span>
+                <br />
+                Sterling Bank Building
+                <br />
+              </address>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Footer;
